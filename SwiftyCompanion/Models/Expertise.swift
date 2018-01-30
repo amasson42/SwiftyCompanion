4228c50599eac7f1f4a7beec9e21d4aae7d1d874
Axel CAROTINE <acarotin@e2r8p10.42.fr>
2018-01-30 16:11:47 +0100

    the student class returns nil if the student is unknown

---
+++ SwiftyCompanion/Models/Expertise.swift
@@ -0,0 +1,15 @@
+//
+//  Expertise.swift
+//  SwiftyCompanion
+//
+//  Created by Axel CAROTINE on 1/30/18.
+//  Copyright © 2018 Arthur MASSON. All rights reserved.
+//
+
+import Foundation
+
+class Expertise {
+    
+     
+    
+}
